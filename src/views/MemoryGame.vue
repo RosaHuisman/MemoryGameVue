58074a13202a108b704e09b0a65be2db5d1fdc29
RosaHuisman <rosa.huisman@gmail.com>
2021-12-22 16:02:22 +0100

    add route memorygame

---
+++ src/views/MemoryGame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="memorygame">
+    <MemoryGame msg="Bienvenue sur le jeu de mémory"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MemoryGame from '@/components/MemoryGame.vue'
+
+export default {
+  name: 'MemoryGame',
+  components: {
+    MemoryGame
+  }
+}
+</script>
