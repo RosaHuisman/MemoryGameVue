6bff4df5a3c1506f4334f3eadd836c82993e5c38
RosaHuisman <rosa.huisman@gmail.com>
2022-01-21 21:58:28 +0100

    media quieries part 2

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,9 +2,6 @@
   <div class="header">
     <h1 class="header_title"> Memory Game </h1>
     <div class="header_scores">
-      <div v-if="$store.state.allScores.length === 0">
-        <span> Meilleur score : pas encore de partie finie </span>
-      </div>
       <div v-if="$store.state.allScores.length > 0">
         <span> Meilleur score : {{ Math.min(...$store.state.allScores) }}</span>
       </div>
